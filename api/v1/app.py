02413b4ab6063894f13bf28ed8b7795dade75437
Ken-Mens <kenneth.mensah@gmail.com>
2019-09-11 00:32:00 +0000

    made changes to app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
