8fe941ee4a92e42fe82d84158cabf7345811a14a
Ken-Mens <kenneth.mensah@gmail.com>
2019-09-11 02:01:23 +0000

    app added error handling 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -9,10 +9,16 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = int(os.getenv('HBNB_API_PORT', '5000'))
 
+
 @app.teardown_appcontext
 def teardown_app(code):
     """Handles teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found_err(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     app.run(host=host, port=port, threaded=True)
