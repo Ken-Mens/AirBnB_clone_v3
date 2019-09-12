8bebf0910cbf4c5e490cc55bfa5dbe519b711f3e
Ken-Mens <kenneth.mensah@gmail.com>
2019-09-12 17:53:09 +0000

    added app pep8 syntax

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,12 +7,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = int(os.getenv('HBNB_API_PORT', '5000'))
+port = os.getenv('HBNB_API_PORT', '5000')
 
 
 @app.teardown_appcontext
 def teardown_app(code):
-    """Handles teardown"""
+    """Handles teardown of storage"""
     storage.close()
 
 
