eefbb35f156e87b35bc4056f0beaaac948459143
Ken-Mens <kenneth.mensah@gmail.com>
2019-09-12 18:17:44 +0000

    pep8 app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
+""" contains teardown and error handler"""
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', '5000')
+app.register_blueprint(app_views)
+host = getenv("HBNB_API_HOST")
+port = getenv("HBNB_API_PORT")
 
 
 @app.teardown_appcontext
-def teardown_app(code):
+def teardown_app(exception):
     """Handles teardown of storage"""
     storage.close()
 
