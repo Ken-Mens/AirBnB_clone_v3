aa5f6133bd76ebf3e1baab55a88a629c39b43d43
Ken-Mens <kenneth.mensah@gmail.com>
2019-09-15 22:25:10 +0000

    add CORS access control

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ contains teardown and error handler"""
-from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
 host = getenv("HBNB_API_HOST")
 port = getenv("HBNB_API_PORT")
 
